237a2bb15269fa7222db852e196d6f7d4cdb7724
Vitor Henrique Dunzer <42549787+SirDunzer@users.noreply.github.com>
2018-10-07 21:58:53 -0300

    Add files via upload

--- Atributos.java
+++ Atributos.java
@@ -1,6 +1,3 @@
-/*
-* Essa classe serÃ¡ herdada pela classe personagem
-*/
 
 public class Atributos {
     private int forca;
@@ -9,11 +6,19 @@ public class Atributos {
     private int inteligencia;
     private int sabedoria;
     private int carisma;
+    Dados d = new Dados();
 
 
-    public Atributos(int forca, int destreza, int constituicao, int inteligencia, int sabedoria, int carisma, Raca race){
+    public Atributos(Classe_Raca.Raca raca){
 
-	    switch(race){
+        forca = d.rolarValores();
+        destreza = d.rolarValores();
+        constituicao = d.rolarValores();
+        inteligencia = d.rolarValores();
+        sabedoria = d.rolarValores();
+        carisma = d.rolarValores();
+
+		switch(raca){
 
 	        case ANAO:
 	            constituicao += 2;
@@ -32,217 +37,216 @@ public class Atributos {
 	            carisma += 1;
 	        break;
 	    }
-
-        this.forca = forca;
-        this.destreza = destreza;
-        this.constituicao = constituicao;
-        this.inteligencia = inteligencia;
-        this.sabedoria = sabedoria;
-        this.carisma = carisma;
     }
 
 		/*GETTERS*/
     public int getForca() {
-    	return this.forca;
+    	return forca;
     }
 
-    public int getdestreza() {
-        return this.destreza;
+    public int getDestreza() {
+        return destreza;
     }
 
     public int getConstituicao() {
-        return this.constituicao;
+        return constituicao;
     }
 
     public int getInteligencia() {
-        return this.inteligencia;
+        return inteligencia;
     }
 
     public int getSabedoria() {
-        return this.sabedoria;
+        return sabedoria;
     }
 
     public int getCarisma() {
-        return this.carisma;
+        return carisma;
     }
 
 	@Override
 	public String toString(){
-		return "Força: "+this.forca+"\nDestreza: "+this.destreza+"\nConstituição: "+this.constituicao+"\nSabedoria: "+this.sabedoria+"\nInteligência: "+this.inteligencia;
+		return "Força: "+forca+"\nDestreza: "+destreza+"\nConstituição: "+constituicao+"\nInteligência: "+inteligencia+"\nSabedoria: "+sabedoria+"\nCarisma: "+carisma+"\n";
 	}
 
 	public int modificadorForca(){
-		if(this.forca == 1){
-			return -5;
-		}else if(this.forca == 2 || this.forca == 3){
-			return -4;
-		}else if(this.forca == 4 || this.forca == 5){
-			return -3;
-		}else if(this.forca == 6 || this.forca == 7){
-			return -2;
-		}else if(this.forca == 8 || this.forca == 9){
-			return -1;
-		}else if(this.forca == 10|| this.forca == 11){
-			return 0;
-		}else if(this.forca == 12 || this.forca == 13){
-			return 1;
-		}else if(this.forca == 14 || this.forca == 15){
-			return 2;
-		}else if(this.forca == 16 || this.forca == 17){
-			return 3;
-		}else if(this.forca == 18 || this.forca == 19){
-			return 4;
-		}else if(this.forca == 20){
-			return 5;
-		}else if(this.forca > 20){
-			return 6;
+
+		switch(this.forca){
+			case 1:
+				return -5;
+			case 2: case 3:
+				return -4;
+			case 4: case 5:
+				return -3;
+			case 6: case 7:
+				return -2;
+			case 8: case 9:
+				return -1;
+			case 10: case 11:
+				return 0;
+			case 12: case 13:
+				return 1;
+			case 14: case 15:
+				return 2;
+			case 16: case 17:
+				return 3;
+			case 18: case 19:
+				return 4;
+			case 20:
+				return 5;
+			default:
+				return 6;
 		}
-        return 0;
 	}
 
 	public int modificadorDestreza(){
-		if(this.destreza == 1){
-			return -5;
-		}else if(this.destreza == 2 || this.destreza == 3){
-			return -4;
-		}else if(this.destreza == 4 || this.destreza == 5){
-			return -3;
-		}else if(this.destreza == 6 || this.destreza == 7){
-			return -2;
-		}else if(this.destreza == 8 || this.destreza == 9){
-			return -1;
-		}else if(this.destreza == 10|| this.destreza == 11){
-			return 0;
-		}else if(this.destreza == 12 || this.destreza == 13){
-			return 1;
-		}else if(this.destreza == 14 || this.destreza == 15){
-			return 2;
-		}else if(this.destreza == 16 || this.destreza == 17){
-			return 3;
-		}else if(this.destreza == 18 || this.destreza == 19){
-			return 4;
-		}else if(this.destreza == 20){
-			return 5;
-		}else if(this.destreza > 20){
-			return 6;
+
+		switch(this.destreza){
+			case 1:
+				return -5;
+			case 2: case 3:
+				return -4;
+			case 4: case 5:
+				return -3;
+			case 6: case 7:
+				return -2;
+			case 8: case 9:
+				return -1;
+			case 10: case 11:
+				return 0;
+			case 12: case 13:
+				return 1;
+			case 14: case 15:
+				return 2;
+			case 16: case 17:
+				return 3;
+			case 18: case 19:
+				return 4;
+			case 20:
+				return 5;
+			default:
+				return 6;
 		}
-        return 0;
 	}
 
 	public int modificadorConstituicao(){
-		if(this.constituicao == 1){
-			return -5;
-		}else if(this.constituicao == 2 || this.constituicao == 3){
-			return -4;
-		}else if(this.constituicao == 4 || this.constituicao == 5){
-			return -3;
-		}else if(this.constituicao == 6 || this.constituicao == 7){
-			return -2;
-		}else if(this.constituicao == 8 || this.constituicao == 9){
-			return -1;
-		}else if(this.constituicao == 10|| this.constituicao == 11){
-			return 0;
-		}else if(this.constituicao == 12 || this.constituicao == 13){
-			return 1;
-		}else if(this.constituicao == 14 || this.constituicao == 15){
-			return 2;
-		}else if(this.constituicao == 16 || this.constituicao == 17){
-			return 3;
-		}else if(this.constituicao == 18 || this.constituicao == 19){
-			return 4;
-		}else if(this.constituicao == 20){
-			return 5;
-		}else if(this.constituicao > 20){
-			return 6;
+
+		switch(this.constituicao){
+			case 1:
+				return -5;
+			case 2: case 3:
+				return -4;
+			case 4: case 5:
+				return -3;
+			case 6: case 7:
+				return -2;
+			case 8: case 9:
+				return -1;
+			case 10: case 11:
+				return 0;
+			case 12: case 13:
+				return 1;
+			case 14: case 15:
+				return 2;
+			case 16: case 17:
+				return 3;
+			case 18: case 19:
+				return 4;
+			case 20:
+				return 5;
+			default:
+				return 6;
 		}
-        return 0;
 	}
 
 	public int modificadorInteligencia(){
-		if(this.inteligencia == 1){
-			return -5;
-		}else if(this.inteligencia == 2 || this.inteligencia == 3){
-			return -4;
-		}else if(this.inteligencia == 4 || this.inteligencia == 5){
-			return -3;
-		}else if(this.inteligencia == 6 || this.inteligencia == 7){
-			return -2;
-		}else if(this.inteligencia == 8 || this.inteligencia == 9){
-			return -1;
-		}else if(this.inteligencia == 10|| this.inteligencia == 11){
-			return 0;
-		}else if(this.inteligencia == 12 || this.inteligencia == 13){
-			return 1;
-		}else if(this.inteligencia == 14 || this.inteligencia == 15){
-			return 2;
-		}else if(this.inteligencia == 16 || this.inteligencia == 17){
-			return 3;
-		}else if(this.inteligencia == 18 || this.inteligencia == 19){
-			return 4;
-		}else if(this.inteligencia == 20){
-			return 5;
-		}else if(this.inteligencia > 20){
-			return 6;
+
+		switch(this.inteligencia){
+			case 1:
+				return -5;
+			case 2: case 3:
+				return -4;
+			case 4: case 5:
+				return -3;
+			case 6: case 7:
+				return -2;
+			case 8: case 9:
+				return -1;
+			case 10: case 11:
+				return 0;
+			case 12: case 13:
+				return 1;
+			case 14: case 15:
+				return 2;
+			case 16: case 17:
+				return 3;
+			case 18: case 19:
+				return 4;
+			case 20:
+				return 5;
+			default:
+				return 6;
 		}
-        return 0;
 	}
 
 	public int modificadorSabedoria(){
-		if(this.sabedoria == 1){
-			return -5;
-		}else if(this.sabedoria == 2 || this.sabedoria == 3){
-			return -4;
-		}else if(this.sabedoria == 4 || this.sabedoria == 5){
-			return -3;
-		}else if(this.sabedoria == 6 || this.sabedoria == 7){
-			return -2;
-		}else if(this.sabedoria == 8 || this.sabedoria == 9){
-			return -1;
-		}else if(this.sabedoria == 10|| this.sabedoria == 11){
-			return 0;
-		}else if(this.sabedoria == 12 || this.sabedoria == 13){
-			return 1;
-		}else if(this.sabedoria == 14 || this.sabedoria == 15){
-			return 2;
-		}else if(this.sabedoria == 16 || this.sabedoria == 17){
-			return 3;
-		}else if(this.sabedoria == 18 || this.sabedoria == 19){
-			return 4;
-		}else if(this.sabedoria == 20){
-			return 5;
-		}else if(this.sabedoria > 20){
-			return 6;
+
+		switch(this.sabedoria){
+			case 1:
+				return -5;
+			case 2: case 3:
+				return -4;
+			case 4: case 5:
+				return -3;
+			case 6: case 7:
+				return -2;
+			case 8: case 9:
+				return -1;
+			case 10: case 11:
+				return 0;
+			case 12: case 13:
+				return 1;
+			case 14: case 15:
+				return 2;
+			case 16: case 17:
+				return 3;
+			case 18: case 19:
+				return 4;
+			case 20:
+				return 5;
+			default:
+				return 6;
 		}
-        return 0;
 	}
 
 	public int modificadorCarisma(){
-		if(this.carisma == 1){
-			return -5;
-		}else if(this.carisma == 2 || this.carisma == 3){
-			return -4;
-		}else if(this.carisma == 4 || this.carisma == 5){
-			return -3;
-		}else if(this.carisma == 6 || this.carisma == 7){
-			return -2;
-		}else if(this.carisma == 8 || this.carisma == 9){
-			return -1;
-		}else if(this.carisma == 10|| this.carisma == 11){
-			return 0;
-		}else if(this.carisma == 12 || this.carisma == 13){
-			return 1;
-		}else if(this.carisma == 14 || this.carisma == 15){
-			return 2;
-		}else if(this.carisma == 16 || this.carisma == 17){
-			return 3;
-		}else if(this.carisma == 18 || this.carisma == 19){
-			return 4;
-		}else if(this.carisma == 20){
-			return 5;
-		}else if(this.carisma > 20){
-			return 6;
+
+		switch(this.carisma){
+			case 1:
+				return -5;
+			case 2: case 3:
+				return -4;
+			case 4: case 5:
+				return -3;
+			case 6: case 7:
+				return -2;
+			case 8: case 9:
+				return -1;
+			case 10: case 11:
+				return 0;
+			case 12: case 13:
+				return 1;
+			case 14: case 15:
+				return 2;
+			case 16: case 17:
+				return 3;
+			case 18: case 19:
+				return 4;
+			case 20:
+				return 5;
+			default:
+				return 6;
 		}
-        return 0;
 	}
 
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,113 @@
+
+import java.util.Scanner;
+
 /*
  * Nessa classe é onde será instanciado o personagem e onde ocorrerá todo o loop de execução
  */
 
 public class Main {
+
     public static void main(String[] args) throws Exception{
-		Personagem Dunzer = new Personagem("Dunzer",1,2,3,4,5,6, Raca.HUMANO, Personagem.Classe.MAGO);
-		System.out.println(Dunzer.toString());
-		Folha.Arquivo(Dunzer);
+        menu();
     }
 
+
+    public static void menu(){
+        int numeroFuncao;
+        Scanner scan = new Scanner(System.in);
+				int numeroJogadores = 1;
+        	System.out.println("======== Gerenciador RPG ========");
+					System.out.println("\nQuantos jogadores na grupo?\n : ");
+					numeroJogadores = scan.nextInt();
+					System.out.flush();
+
+					System.out.println("======== Gerenciador RPG ========");
+        	System.out.println("\n\n1 - Criar Personagem");
+					System.out.println("\n\n0 - Sair");
+
+        numeroFuncao = scan.nextInt();
+        while(numeroFuncao != 0){
+					switch(numeroFuncao){
+						case 0:
+					}
+			Personagem Herois = criaPersonagem(numeroJogadores);
+    	}
+
+
+		public static Personagem criaPersonagem(int numeroJogadores){
+
+			Scanner scan = new Scanner(System.in);
+			System.out.println("======== Criar Personagem ========");
+
+			System.out.print("Digite o nome do seu Personagem: ");
+			String nome = scan.nextLine();
+
+			System.out.println("\nEscolha a Raca: ");
+			System.out.println("1 - Anão\n2 - Humano\n3 - Elfo");
+			int escolha = scan.nextInt();
+			Personagem.Raca raca;
+			switch(escolha){
+				case 1:
+					raca = Personagem.Raca.ANAO;
+					break;
+				case 2:
+					raca = Personagem.Raca.HUMANO;
+					break;
+				case 3:
+					raca = Personagem.Raca.ELFO;
+					break;
+				default:
+					raca = null
+					break;
+			}
+
+			System.out.print("\nEscolha a Classe: ");
+			System.out.println("1 - Barbaro\n2 - Caçador\n3 - Guerreiro\n4 - Ladino\n5 - Mago");
+			escolha = scan.nextInt();
+			Personagem.Classe classe;
+			switch(escolha){
+				case 1:
+					classe = Personagem.Classe.BARBARO;
+					break;
+				case 2:
+					 classe = Personagem.Classe.CACADOR;
+					 break;
+				case 3:
+					classe = Personagem.Classe.GUERREIRO;
+					break;
+				case 4:
+					 classe = Personagem.Classe.LADINO;
+					 break;
+				case 5:
+					 classe = Personagem.Classe.MAGO;
+					 break;
+				default:
+					 classe = null
+					 break;
+			}
+
+			Personagem heroi = new Personagem[numeroJogadores];
+			heroi = new Personagem(nome,raca,classe);
+			return heroi;
+			// System.out.println(heroi[i].toString());
+		}
+
+
+
+
 }
+//            Dados D = new Dados();
+//
+//            String teste = "MAGO";
+//
+//            Personagem Jogador1 = new Personagem(nome, Raca.HUMANO, Personagem.Classe.valueOf(teste));
+//            Personagem Miguel = new Personagem("Miguel",Raca.HUMANO, Personagem.Classe.BARBARO);
+//
+//            System.out.println(Jogador1.toString());
+//            System.out.println(Miguel.toString());
+//
+//            Personagem.habilidadeEspecial(Miguel, Jogador1, Equipamentos.Armas.ESPADALONGA);
+//
+//            System.out.println(Jogador1.getHP_Atual());
+//            Folha.salvaInformacoes(Jogador1);
+//            Folha.exportaPersonagem(Jogador1);
