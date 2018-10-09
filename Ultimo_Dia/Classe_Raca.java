de6ebf9fa0916e1a9241a551c313de07cfcb6f00
Miguel Nunes <miguel.alfredo.nunes@gmail.com>
2018-10-09 12:32:42 -0300

    Add files via upload

--- Ultimo_Dia/Classe_Raca.java
+++ Ultimo_Dia/Classe_Raca.java
@@ -1,12 +1,3 @@
-/*
-* Essa será a classe abstrata da qual personagem irá herdar
-*/
-
-// Overload == Dois ou mais métodos ou construtores com o mesmo nome
-// mas assinaturas diferentes
-
-// Override == Redefinir um método herdado de alguma super classe
-
 public interface Classe_Raca{
 
     public enum Classe{
@@ -24,17 +15,17 @@ public interface Classe_Raca{
     }
 
 
-	// Habilidade de ataque, afeta outro PC
- 	//void habilidadeEspecial(Personagem Atacante, Personagem Defensor);
-	// Habilidade passiva, afeta somente a si próprio
-        //void habilidadeEspecial(Personagem Proprio); // Overload
-	int calcularHP(Classe classe_escolhida);
-	/*
-	* Habilidades de cada classe:
-	*	Barbaro: Ataque Furioso, ataque dobra o bonus e adiciona mais 1d12 de dano
-	*	Caçador: Ataque Preciso, quando atacar rola 2d20, seleciona o maior e adiciona 1d6 de dano
-	* 	Guerreiro: Recuperar Folêgo, regenera 1d10 de HP
-	*	Ladino: Ataque Surpresa, alvo rola sabedoria e vc rola destreza, se seu roll for maior vc da 2d6 a mais de dano
-	* 	Mago: Usar uma Magia
-	*/
+    // Habilidade de ataque, afeta outro PC
+//    abstract static void habilidadeEspecial(Personagem Atacante, Personagem Defensor);
+    // Habilidade passiva, afeta somente a si próprio
+    //void habilidadeEspecial(Personagem Proprio); // Overload
+    int calcularHP(Classe classe_escolhida);
+    /*
+    * Habilidades de cada classe:
+    *	Barbaro: Ataque Furioso, ataque dobra o bonus e adiciona mais 1d12 de dano
+    *	Caçador: Ataque Preciso, quando atacar rola 2d20, seleciona o maior e adiciona 1d6 de dano
+    * 	Guerreiro: Recuperar Folêgo, regenera 1d10 de HP
+    *	Ladino: Ataque Surpresa, alvo rola sabedoria e vc rola destreza, se seu roll for maior vc da 2d6 a mais de dano
+    * 	Mago: Usar uma Magia
+    */
 }
--- Ultimo_Dia/Folha.java
+++ Ultimo_Dia/Folha.java
@@ -1,19 +1,28 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 
 public class Folha {
     public static void exportaPersonagem(Personagem Jogador) throws Exception{
         File personagem = new File("Personagem.txt");
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+//        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        FileOutputStream output = new FileOutputStream(personagem, true);
 
-        output.write(Jogador.getNome()); output.newLine();
-        output.write(String.valueOf(Jogador.getForca())); output.newLine();
-        output.write(String.valueOf(Jogador.getDestreza())); output.newLine();
-        output.write(String.valueOf(Jogador.getConstituicao())); output.newLine();
-        output.write(String.valueOf(Jogador.getInteligencia())); output.newLine();
-        output.write(String.valueOf(Jogador.getSabedoria())); output.newLine();
-        output.write(String.valueOf(Jogador.getCarisma())); output.newLine();
+        output.write(Jogador.getNome().getBytes());
+        output.write("\n".getBytes());
+        output.write(String.valueOf(Jogador.getForca()).getBytes()); 
+        output.write("\n".getBytes());
+        output.write(String.valueOf(Jogador.getDestreza()).getBytes());
+        output.write("\n".getBytes());
+        output.write(String.valueOf(Jogador.getConstituicao()).getBytes());
+        output.write("\n".getBytes());
+        output.write(String.valueOf(Jogador.getInteligencia()).getBytes());
+        output.write("\n".getBytes());
+        output.write(String.valueOf(Jogador.getSabedoria()).getBytes());
+        output.write("\n".getBytes());
+        output.write(String.valueOf(Jogador.getCarisma()).getBytes());
+        output.write("\n---".getBytes());
         System.out.println("Arquivo criado com Sucesso!");
     }
 
